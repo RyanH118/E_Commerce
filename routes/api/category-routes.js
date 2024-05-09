d4a2c9442fa5018c0958ef030e5f04cff2aa70dc
ryanh <ryanhinson118@mail.com>
2024-05-09 16:44:35 -0400

    Added routes for tag-routes.js file.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,10 +49,10 @@ router.put('/:id', async (req, res) => {
     const category = await Category.findByPk(req.params.id);
     if (!category) {
       res.status(404).json({ message: 'Category not found' });
-      return;
-    }
-
+    } else {
+      await category.update(req.body);
       res.status(200).json(category);
+    }
     } catch (err) {
     res.status(400).json(err);
   }
